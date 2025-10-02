c01e77b8dcd29907cd8bbabd2f403295d0871238
Carlos Eduardo <cadus824@gmail.com>
2025-10-02 15:11:12 -0300

    fim

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -4,9 +4,34 @@ class Calculadora:
         self.battery: int = 0
         self.display: float = 0
 
-    def _str__(self):
+    def __str__(self):
         return f"display = {self.display:.2f}, battery = {self.battery}"
     
+    def carregar(self,increment: int):
+        self.battery += increment
+        if self.battery > self.batteryMax:
+            self.battery = self.batteryMax
+
+    def soma(self, a: float, b: float) -> None:
+        if self.battery > 0:
+            self.display = a + b
+            self.battery -= 1
+        else:
+            print("fail: bateria insuficiente")
+
+    def divi(self, a: float, b: float) -> None:
+        if self.battery > 0:
+            if b != 0:
+                self.display = a / b
+                self.battery -= 1
+            else:
+                print("fail: divisao por zero")
+                self.battery -= 1
+        else:
+            print("fail: bateria insuficiente")
+
+    
+
 def main():
     calculadora : Calculadora = Calculadora("")
     
@@ -18,6 +43,20 @@ def main():
 
         if args[0] == "end":
             break
+        elif args[0] == "init":
+            batteryMax: int = int(args[1])
+            calculadora = Calculadora(batteryMax)
+        elif args[0] == "charge":
+            increment: int = int(args[1])
+            calculadora.carregar(increment)
+        elif args[0] == "sum":
+            a: float = float(args[1])
+            b: float = float(args[2])
+            calculadora.soma(a,b)
+        elif args[0] == "div":
+            a: float = float(args[1])
+            b: float = float(args[2])
+            calculadora.divi(a,b)
         elif args[0] == "show":
             print(calculadora)
 
